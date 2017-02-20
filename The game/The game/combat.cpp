eb4df7bfab464a5125a6262940dc50e432e76e63
billp0213 <bpeck0213@gmail.com>
2017-02-20 05:07:01 -0500

    test to see how adding classes works

---
+++ The game/The game/combat.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int pLevel = 10;
+	int eLevel = 5;
+
+	if (pLevel > eLevel) {
+		cout << "You win\n";
+	}
+
+	return 0;
+	
+}
