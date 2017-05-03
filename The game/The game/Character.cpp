0110c022fd7a0f9ff44a4ec09e3b6cd2b607e436
billp0213 <bpeck0213@gmail.com>
2017-05-02 20:39:55 -0400

    reworked everything

---
+++ The game/The game/Character.cpp
@@ -0,0 +1,74 @@
+#include "Character.h"
+
+Character::Character(int xPos_in, int yPos_in, int level_in, int multiplier) : xPos(xPos_in), yPos(yPos_in) {
+	set_stats(level_in, multiplier);
+}
+
+int Character::get_xPos() {
+	return xPos;
+}
+
+int Character::get_yPos() {
+	return yPos;
+}
+
+int Character::get_level() {
+	return level;
+}
+
+int * Character::get_stats() {
+	return &stats[0];
+}
+
+void Character::set_level(int level_in) {
+	level = level_in;
+}
+
+void Character::set_pos(int xPos_in, int yPos_in) {
+	xPos = xPos_in;
+	yPos = yPos_in;
+}
+
+void Character::set_stats(int level_in, int multiplier) {
+	level = level_in;
+	stats[0] = 20 + level * multiplier * 10;
+	stats[1] = 1 + level * multiplier;
+	stats[2] = 1 + level * multiplier;
+	stats[3] = 1 + level * multiplier;
+}
+
+Player::Player(int xPos_in, int yPos_in, int level_in, int multiplier, int* aux_in, int exp_in) {
+	exp = exp_in;
+	for (int i = 0; i < 4; i++) {
+		auxStats[i] = *aux_in;
+		aux_in++;
+	}
+}
+
+int * Player::get_auxStats() {
+	return &auxStats[0];
+}
+
+int Player::get_exp() {
+	return exp;
+}
+
+void Player::set_auxStats(int * aux_in) {
+	for (int i = 0; i < 4; i++) {
+		auxStats[i] = *aux_in;
+		aux_in++;
+	}
+	
+}
+
+void Player::add_exp(int exp_in) {
+	exp += exp_in;
+}
+
+void Player::levelUp() {
+	int lvl = get_level();
+	set_level(lvl++);
+	exp = 0;
+	set_stats(lvl, multiplier);
+	
+}
--- The game/The game/character.h
+++ The game/The game/character.h
@@ -3,71 +3,34 @@
 
 class Character {
 	public:
-		Character() {};
-		Character(int xPos_in, int yPos_in, int level_in, int multiplier)
-			: xPos(xPos_in), yPos(yPos_in) {
-			level = level_in;
-			stats[0] = 10 + 2 * multiplier * level_in;
-			stats[1] = multiplier * level_in;
-			stats[2] = multiplier * level_in;
-			stats[3] = multiplier * level_in;
-		} 
+		Character(int xPos_in, int yPos_in, int level_in, int multiplier);
 		int get_xPos();
 		int get_yPos();
 		int get_level();
 		int* get_stats();
-		void set_stats(int level_in, int multiplier) {
-			level = level_in;
-			stats[0] = 10 + level_in * multiplier;
-			stats[1] = level_in * multiplier;
-			stats[2] = level_in * multiplier;
-			stats[3] = level_in * multiplier;
-		}
-		void set_pos(int xPos_in, int yPos_in) {
-			xPos = xPos_in;
-			yPos = yPos_in;
-		}
+		void set_level(int level_in);
+		void set_stats(int level_in, int multiplier);
+		void set_pos(int xPos_in, int yPos_in);
 	private:
 		int xPos;
 		int yPos;
+		int level;
 //hp, attack, defense, speed		
 		int stats[4];
-		int level;
+		
 };
 
 class Player : public Character {
 	public:
-		Player() : Character() {
-			auxStats[0] = 1;
-			auxStats[1] = 1;
-			auxStats[2] = 1;
-			auxStats[3] = 1;
-			Exp = 0;
-		}
-		Player(int xPos_in, int yPos_in, int level_in, int multiplier, int* aux_in, int Exp_in) : Character(xPos_in, yPos_in, level_in, multiplier) {
-			auxStats[0] = aux_in[0];
-			auxStats[1] = aux_in[1];
-			auxStats[2] = aux_in[2];
-			auxStats[3] = aux_in[3];
-			Exp = Exp_in;
-		}
-		int* get_auxStats() {
-			return auxStats;
-		}
-		int get_Exp() {
-			return Exp;
-		}
-		void set_auxStats(int* aux_in) {
-			auxStats[0] = aux_in[0];
-			auxStats[1] = aux_in[1];
-			auxStats[2] = aux_in[2];
-			auxStats[3] = aux_in[3];
-		}
-		void set_Exp(int Exp_in) {
-			Exp = Exp_in;
-		}
+		Player(int xPos_in, int yPos_in, int level_in, int multiplier, int* aux_in, int Exp_in) : Character(xPos_in, yPos_in, level_in, multiplier);
+		int* get_auxStats();
+		int get_exp();
+		void set_auxStats(int* aux_in);
+		void add_exp(int exp_in);
+		void levelUp();
 	private:
+		//intellect, luck, magic, ????
 		int auxStats[4];
-		int Exp;
+		int exp;
 		int multiplier = 1;
 };
